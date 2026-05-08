6535bc51fe0afa2ea292028a06bfda12129bc08d
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-08 07:36:02 +0000

    Created searchcarcard

---
+++ src/components/search_page_components/SearchResults.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import './SearchResults.css'
+
+function SearchResultsCarCard(){
+  return(
+    <div className='searchcarcard'>
+                    <div className='searchcarcardimage'></div>
+                    <div className='searchcardcarinfo'>
+                        <div className='searchcardcarname'>Porsche 911 GT3</div>
+                        <div className='searchcardcarprice'>
+                            $850
+                            <div style={{
+                                color: '#64648B',
+                                fontSize:'15px',
+                                fontWeight: '500'
+                            }}>/day</div>
+                        </div>
+                    </div>
+                    <div className='searchcardcarproperties'>
+                        <div className='searchcardcarproperty'>502 HP</div>
+                        <div className='searchcardcarproperty'>3.2s</div>
+                        <div className='searchcardcarproperty'>MANUAL</div>
+                    </div>
+                    <div className='searchcardcarviewdetails'>
+                        <div className='searchcardviewdetailsbutton'>VIEW DETAILS</div>
+                    </div>
+                </div>
+  )
+}
+
+export default function SearchResults() {
+  return (
+        <div className='searchresults'>
+            <div className='bigsearchbar'>
+                <input type='text' placeholder='Search' className='search'/>
+                <div className='sortby'>
+                    <div className='sortbytitle'>SORT BY</div>
+                    <div className='sortbyprice'>PRICE:</div>
+                </div>
+            </div>
+            <div className='showingsearchresults'>
+                <div style={{display: 'flex',flexDirection: 'row'}}>Showing<div style={{fontWeight:'bold'}}>&nbsp;12&nbsp;</div>results</div>
+                <div className='searchview'>
+                    <div className='tileview'>T</div>
+                    <div className='listview'>L</div>
+                </div>
+        
+            </div>
+            <div className='searchcarcardrow'>
+                <SearchResultsCarCard/>
+                <SearchResultsCarCard/>
+                <SearchResultsCarCard/>
+            </div>    
+            <div className='searchcarcardrow'>
+                <SearchResultsCarCard/>
+                <SearchResultsCarCard/>
+                <SearchResultsCarCard/>
+            </div>    
+
+        </div>
+  )
+}
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,53 +1,13 @@
 import React from 'react'
 import './SearchPage.css'
 import Filter from '../components/search_page_components/Filter'
+import SearchResults from '../components/search_page_components/SearchResults'
 
 export default function SearchPage() {
   return (
     <div className='searchMainBody'>
         <Filter/>
-        <div className='searchresults'>
-            <div className='bigsearchbar'>
-                <input type='text' placeholder='Search' className='search'/>
-                <div className='sortby'>
-                    <div className='sortbytitle'>SORT BY</div>
-                    <div className='sortbyprice'>PRICE:</div>
-                </div>
-            </div>
-            <div className='showingsearchresults'>
-                <div style={{display: 'flex',flexDirection: 'row'}}>Showing<div style={{fontWeight:'bold'}}>&nbsp;12&nbsp;</div>results</div>
-                <div className='searchview'>
-                    <div className='tileview'>T</div>
-                    <div className='listview'>L</div>
-                </div>
-        
-            </div>
-            <div className='searchcarcardrow'>
-                <div className='searchcarcard'>
-                    <div className='searchcarcardimage'></div>
-                    <div className='searchcardcarinfo'>
-                        <div className='searchcardcarname'>Porsche 911 GT3</div>
-                        <div className='searchcardcarprice'>
-                            $850
-                            <div style={{
-                                color: '#64648B',
-                                fontSize:'15px',
-                                fontWeight: '500'
-                            }}>/day</div>
-                        </div>
-                    </div>
-                    <div className='searchcardcarproperties'>
-                        <div className='searchcardcarproperty'></div>
-                        <div className='searchcardcarproperty'></div>
-                        <div className='searchcardcarproperty'></div>
-                    </div>
-                    <div className='searchcardcarviewdetails'>
-                        <div className='searchcardviewdetailsbutton'>VIEW DETAILS</div>
-                    </div>
-                </div>
-            </div>    
-
-        </div>
+        <SearchResults/>
     </div>
   )
 }
