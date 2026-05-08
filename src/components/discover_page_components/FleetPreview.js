0d5e58060e0e44a2df64141407f0e4be1cb465bc
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-08 12:43:42 +0000

    Created dynamic cards for discovery page

--- src/components/discover_page_components/FleetPreview.js
+++ src/components/discover_page_components/FleetPreview.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import './FleetPreview.css'
 
-function FleetPreviewCard(){
+function FleetPreviewCard({name, type, title, amount, duration, mph, engine, transmission}){
     return(
         <div className='fleetpreviewcard'>
                 <div className='fleetpreviewimage'></div>
                 <div className='fleetpreviewcartype'>
-                    <div className='fleetpreviewcartypename'>LUXURY SEDAN</div>
-                    <div className='fleetpreviewcartypesale'>RENTAL</div>                    
+                    <div className='fleetpreviewcartypename'>{name}</div>
+                    <div className='fleetpreviewcartypesale'>{type}</div>                    
                 </div>
                 <div className='fleetpreviewcarname'>
-                    <div className='fleetpreviewcartitle'>Stratus Executive</div>
+                    <div className='fleetpreviewcartitle'>{title}</div>
                     <div className='fleetpreviewcarprice'>
-                        <div className='fleetpreviewcarpriceamount'>$850</div>
-                        <div className='fleetpreviewcarduration'>/DAY</div>
+                        <div className='fleetpreviewcarpriceamount'>${amount}</div>
+                        <div className='fleetpreviewcarduration'>{duration}</div>
                     </div>
                 </div>
                 <div className='fleetpreviewcarproperties'>
-                    <div>195 MPH</div>
-                    <div>HYBRID</div>
-                    <div>AUTO</div>
+                    <div>{mph} MPH</div>
+                    <div>{engine}</div>
+                    <div>{transmission}</div>
                 </div>
                 <div className='fleetpreviewdetails'>
                     <div className='fleetpreviewdetailsbutton'>VIEW DETAILS</div>
@@ -29,6 +29,14 @@ function FleetPreviewCard(){
 }
 
 export default function FleetPreview() {
+    const nameList = ['SEDAN', 'LEXUS', 'FERRARI']
+    const typeList = ['RENTAL', 'SALE', 'SALE']
+    const titleList = ['Stratus Executive', 'Ferrari', 'Bugatti']
+    const amountList = ['800', '800,000', '1,000,000']
+    const durationList = ['/DAY', '', '']
+    const mphList = ['195', '200', '250']
+    const engineList = ['HYBRID', 'ELECTRIC', 'GAS']
+    const transmissionList = ['AUTO', 'MECHANICAL', 'AUTO']
   return (
     <div className='fleetpreviewmainbody'>
         <div className='fleetpreviewbar'>
@@ -36,9 +44,9 @@ export default function FleetPreview() {
             <div className='fleetpreviewviewall'>VIEW ALL INVENTORY</div>
         </div>
         <div className='fleetpreviewcardsdiv'>
-            <FleetPreviewCard/>
-            <FleetPreviewCard/>
-            <FleetPreviewCard/>
+            <FleetPreviewCard name={nameList[0]} type={typeList[0]} title={titleList[0]} amount={amountList[0]} duration={durationList[0]} mph={mphList[0]} engine={engineList[0]} transmission={transmissionList[0]}/>
+            <FleetPreviewCard name={nameList[1]} type={typeList[1]} title={titleList[1]} amount={amountList[1]} duration={durationList[1]} mph={mphList[1]} engine={engineList[1]} transmission={transmissionList[1]}/>
+            <FleetPreviewCard name={nameList[2]} type={typeList[2]} title={titleList[2]} amount={amountList[2]} duration={durationList[2]} mph={mphList[2]} engine={engineList[2]} transmission={transmissionList[2]}/>
         </div>
     </div>
   )
--- src/components/discover_page_components/Spotlight.js
+++ src/components/discover_page_components/Spotlight.js
@@ -2,19 +2,21 @@ import React from 'react'
 import './Spotlight.css'
 
 export default function Spotlight() {
+    const spotlightCarName = 'Ferrari'
+    const spotlightCarDescription = 'A very fast car'
+    const spotlightCarSaleType = 'RENT'
+    const spotlightCarPrice = '2500/DAY'
   return (
     <div className='spotlightmainbody'>
         <div className='spotlightinfo'>
             <div className='spotlightheader'>Spotlight</div>
-            <div className='spotlightcarname'>Car Name</div>
+            <div className='spotlightcarname'>{spotlightCarName}</div>
             <div className='spotlightcardescription'>
-                SpotlightSpotlightSpotlightSpotlightSpotlightSpotlightSpotlight
-                SpotlightSpotlightSpotlightSpotlightSpotlightSpotlightSpotlight
-                SpotlightSpotlightSpotlightSpotlightSpotlightSpotlightSpotlight
+                {spotlightCarDescription}
             </div>
             <div className='spotlightcarpricing'>
-                <div className='spotlightcarsaletype'>FOR&nbsp;SALE</div>
-                <div className='spotlightcarprice'>$2500</div>
+                <div className='spotlightcarsaletype'>FOR&nbsp;{spotlightCarSaleType}</div>
+                <div className='spotlightcarprice'>${spotlightCarPrice}</div>
             </div>
             <div className='spotlightcardetailsbuttondiv'>
                 <div className='spotlightcardetailsbutton'>See&nbsp;More</div>
