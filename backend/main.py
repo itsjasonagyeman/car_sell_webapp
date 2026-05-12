77c1902dff1da730e40a92fae764d6bc458753dc
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-12 09:45:48 +0000

    Added dynamic mapping from db in discovery page

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from routers.carrouter import router
 from database.dbsetup import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
-app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router, prefix='/cars')
--- frontend/src/components/discover_page_components/FleetPreview.js
+++ frontend/src/components/discover_page_components/FleetPreview.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './FleetPreview.css'
 import { Link } from 'react-router-dom'
 
-function FleetPreviewCard({name, type, title, amount, duration, mph, engine, transmission}){
+function FleetPreviewCard({image, primaryname, saletype, secondaryname, price, duration, speed, enginetype, geartype}){
     return(
         <div className='fleetpreviewcard'>
-                <div className='fleetpreviewimage'></div>
+                <div className='fleetpreviewimage' style={{backgroundColor: image}}></div>
                 <div className='fleetpreviewcartype'>
-                    <div className='fleetpreviewcartypename'>{name}</div>
-                    <div className='fleetpreviewcartypesale'>{type}</div>                    
+                    <div className='fleetpreviewcartypename'>{primaryname}</div>
+                    <div className='fleetpreviewcartypesale'>{saletype}</div>                    
                 </div>
                 <div className='fleetpreviewcarname'>
-                    <div className='fleetpreviewcartitle'>{title}</div>
+                    <div className='fleetpreviewcartitle'>{secondaryname}</div>
                     <div className='fleetpreviewcarprice'>
-                        <div className='fleetpreviewcarpriceamount'>${amount}</div>
+                        <div className='fleetpreviewcarpriceamount'>${price}</div>
                         <div className='fleetpreviewcarduration'>{duration}</div>
                     </div>
                 </div>
                 <div className='fleetpreviewcarproperties'>
-                    <div>{mph} MPH</div>
-                    <div>{engine}</div>
-                    <div>{transmission}</div>
+                    <div>{speed} MPH</div>
+                    <div>{enginetype}</div>
+                    <div>{geartype}</div>
                 </div>
                 <div className='fleetpreviewdetails'>
                     <Link to='/cardetails' className='fleetpreviewdetailsbutton'>VIEW DETAILS</Link>
@@ -30,24 +30,35 @@ function FleetPreviewCard({name, type, title, amount, duration, mph, engine, tra
 }
 
 export default function FleetPreview() {
-    const nameList = ['SEDAN', 'LEXUS', 'FERRARI']
-    const typeList = ['RENTAL', 'SALE', 'SALE']
-    const titleList = ['Stratus Executive', 'Ferrari', 'Bugatti']
-    const amountList = ['800', '800,000', '1,000,000']
-    const durationList = ['/DAY', '', '']
-    const mphList = ['195', '200', '250']
-    const engineList = ['HYBRID', 'ELECTRIC', 'GAS']
-    const transmissionList = ['AUTO', 'MECHANICAL', 'AUTO']
+    const [cars, setCars] = useState([])
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/cars/car')
+        .then(res => res.json())
+        .then(data => setCars(data.slice(0,3)))
+        .catch(err => console.log(err))
+    }, [])
+
   return (
     <div className='fleetpreviewmainbody'>
         <div className='fleetpreviewbar'>
             <div className='fleetpreviewtitle'>Available Fleet</div>
-            <div className='fleetpreviewviewall'>VIEW ALL INVENTORY</div>
+            <Link to='/search' className='fleetpreviewviewall'>VIEW ALL INVENTORY</Link>
         </div>
         <div className='fleetpreviewcardsdiv'>
-            <FleetPreviewCard name={nameList[0]} type={typeList[0]} title={titleList[0]} amount={amountList[0]} duration={durationList[0]} mph={mphList[0]} engine={engineList[0]} transmission={transmissionList[0]}/>
-            <FleetPreviewCard name={nameList[1]} type={typeList[1]} title={titleList[1]} amount={amountList[1]} duration={durationList[1]} mph={mphList[1]} engine={engineList[1]} transmission={transmissionList[1]}/>
-            <FleetPreviewCard name={nameList[2]} type={typeList[2]} title={titleList[2]} amount={amountList[2]} duration={durationList[2]} mph={mphList[2]} engine={engineList[2]} transmission={transmissionList[2]}/>
+            {cars.map((car,index)=>{
+                return <FleetPreviewCard
+                key={car.id}
+                image={car.image}
+                primaryname = {car.primary_name}
+                secondaryname={car.secondary_name}
+                saletype={car.sale_type}
+                price={car.price}
+                duration= '/DAY'
+                speed={car.speed}
+                enginetype={car.engine_type}
+                geartype={car.gear_type}
+                />
+            })}
         </div>
     </div>
   )
