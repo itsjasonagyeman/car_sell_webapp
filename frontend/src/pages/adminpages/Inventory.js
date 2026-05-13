e5fc3d2314d73ddb949556dcc1a96876fb780b64
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-13 15:34:20 +0000

    Finished Admin Inventory UI

--- frontend/src/pages/adminpages/Inventory.js
+++ frontend/src/pages/adminpages/Inventory.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import './Inventory.css'
 
+function InventoryCarCard(){
+    return(
+        <div className='inventorycarcard'>
+                <div className='inventoryvehicle'>
+                    <div className='inventoryvehicleimage'></div>
+                    <div className='inventoryvehicleinfo'>
+                        <div className='inventoryvehicleprimaryname'>Ferrari</div>
+                        <div className='inventoryvehiclesecondaryname'>Ferraririr</div>
+                    </div>
+                </div>
+                <div className='inventorystatus'>AVAILABLE</div>
+                <div className='inventoryprice'>$3000</div>
+                <div className='inventorycategory'>Supercar</div>
+                <div className='inventoryactions'>
+                    <div className='inventoryupdate'>UPDATE</div>
+                    <div className='inventorydelete'></div>
+                </div>
+            </div>
+    )
+}
+
+function InventoryPropertyCard(){
+    return(
+        <div className='inventoryproperty'>
+                <div className='inventorypropertyfigure'>32</div>
+                <div className='inventorypropertyname'>Total Vehicles</div>
+        </div>
+    )
+}
+
 export default function Inventory() {
   return (
     <div className='maininventorybody'>
@@ -27,22 +57,24 @@ export default function Inventory() {
                 <div className='actionsheader'>ACTIONS</div>
             </div>
 
-            <div className='inventorycarcard'>
-                <div className='inventoryvehicle'>
-                    <div className='inventoryvehicleimage'></div>
-                    <div className='inventoryvehicleinfo'>
-                        <div className='inventoryvehicleprimaryname'>Ferrari</div>
-                        <div className='inventoryvehiclesecondaryname'>Ferraririr</div>
-                    </div>
-                </div>
-                <div className='inventorystatus'>AVAILABLE</div>
-                <div className='inventoryprice'>$3000</div>
-                <div className='inventorycategory'>Supercar</div>
-                <div className='inventoryactions'>
-                    <div className='inventoryupdate'>UPDATE</div>
-                    <div className='inventorydelete'></div>
-                </div>
+            <InventoryCarCard/>
+        </div>
+        <div className='inventoryresultsdiv'>
+            <div className='showingresults'>Showing 1 to 4 of 32 results</div>
+            <div className='changeinventorydiv'>
+                <div className='previousinventorybutton'>Previous</div>
+                <div className='resultsbutton'>1</div>
+                <div className='resultsbutton'>2</div>
+                <div className='resultsbutton'>3</div>
+                <div className='nextinventorybutton'>Next</div>
             </div>
+        </div>
+
+        <div className='inventorypropertiesdiv'>
+            <InventoryPropertyCard/>
+            <InventoryPropertyCard/>
+            <InventoryPropertyCard/>
+            <InventoryPropertyCard/>
         </div>
     </div>
   )
