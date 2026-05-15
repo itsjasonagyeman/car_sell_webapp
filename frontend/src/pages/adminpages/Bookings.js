0e68ba274f1eb5cb28e2e878a893324fe5d5515b
Jason Nhyira Agyeman <agyemanjasonnhyira@gmail.com>
2026-05-15 16:50:40 +0000

    Minor UI tweaks to bookings

--- frontend/src/pages/adminpages/Bookings.js
+++ frontend/src/pages/adminpages/Bookings.js
@@ -1,27 +1,9 @@
 import React from 'react'
 import './Bookings.css'
 
-export default function Bookings() {
-  return (
-    <div className='mainbookingsbody'>
-      <div className='bookingstitle'>Bookings</div>
-      <div className='bookingscarddiv'>
-        <div className='bookingscard'>
-          <div className='bookingscardtitle'>TOTAL ACTIVE BOOKINGS</div>
-          <div className='bookingscardvalue'>42</div>
-        </div>
-      </div>
-      <div className='bookingsfilterdiv'></div>
-      <div className='bookingscustomertable'>
-        <div className='bookingscustomertabletitles'>
-          <div className='bookingscustomer'>CUSTOMER</div>
-          <div className='bookingsvehicle'>VEHICLE</div>
-          <div className='bookingsperiod'>BOOKING PERIOD</div>
-          <div className='bookingsstatus'>STATUS</div>
-          <div className='bookingsrevenue'>REVENUE</div>
-          <div className='bookingsactions'>ACTIONS</div>
-        </div>
-        <div className='bookingscustomercard'>
+function CustomerCard(){
+  return(
+    <div className='bookingscustomercard'>
           <div className='bookingscardcustomer'>
             <div className='bookingscardcustomerimage'></div>
             <div className='bookingscardcustomerinfo'>Jason Agyeman</div>
@@ -34,7 +16,41 @@ export default function Bookings() {
           <div className='bookingscardstatus'>In Progress</div>
           <div className='bookingscardrevenue'>$12,000</div>
           <div className='bookingscardactions'></div>
+    </div>
+  )
+}
+
+function BookingsPropertyCard(){
+  return(
+    <div className='bookingscard'>
+      <div className='bookingscardtitle'>TOTAL ACTIVE BOOKINGS</div>
+      <div className='bookingscardvalue'>42</div>
+    </div>
+  )
+}
+
+export default function Bookings() {
+  return (
+    <div className='mainbookingsbody'>
+      <div className='bookingstitle'>Bookings</div>
+      <div className='bookingscarddiv'>
+        <BookingsPropertyCard/>
+        <BookingsPropertyCard/>
+        <BookingsPropertyCard/>
+        <BookingsPropertyCard/>
+      </div>
+      <div className='bookingsfilterdiv'></div>
+      <div className='bookingscustomertable'>
+        <div className='bookingscustomertabletitles'>
+          <div className='bookingscustomer'>CUSTOMER</div>
+          <div className='bookingsvehicle'>VEHICLE</div>
+          <div className='bookingsperiod'>BOOKING PERIOD</div>
+          <div className='bookingsstatus'>STATUS</div>
+          <div className='bookingsrevenue'>REVENUE</div>
+          <div className='bookingsactions'>ACTIONS</div>
         </div>
+        <CustomerCard/>
+        <CustomerCard/>
       </div>
     </div>
   )
